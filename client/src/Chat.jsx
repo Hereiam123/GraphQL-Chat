1be0bda75734fe70c183cf10190b2e0b8fe73ff1
Brian De Maio <bdemaio1@gmail.com>
2020-08-19 01:02:41 -0400

    Getting JSON data from server in Messages component

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,12 +1,40 @@
 import React from "react";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/",
   cache: new InMemoryCache(),
 });
 
-const Chat = () => <div>I'm the chat window</div>;
+const GET_MESSAGES = gql`
+  query {
+    messages {
+      id
+      content
+      user
+    }
+  }
+`;
+
+const Messages = ({ user }) => {
+  const { data } = useQuery(GET_MESSAGES);
+  if (!data) {
+    return null;
+  }
+  return JSON.stringify(data);
+};
+
+const Chat = () => (
+  <div>
+    <Messages user="Me" />
+  </div>
+);
 
 export default () => (
   <ApolloProvider client={client}>
